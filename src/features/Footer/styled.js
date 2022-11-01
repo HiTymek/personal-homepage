2ed799f13c8d06013ae5e08d4cfff41b1418a333
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-11-01 19:09:24 +0100

    add better code formating

--- src/features/Footer/styled.js
+++ src/features/Footer/styled.js
@@ -1,37 +1,37 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-max-width: 50%;
-margin: 120px 0 120px 0;
-color: #252525;
+    max-width: 50%;
+    margin: 120px 0 120px 0;
+    color: #252525;
 `;
 
 export const LetsTalk = styled.p`
-color: #6E7E91;
-font-weight: 700;
-font-size: 12px;
-line-height: 1.3;
-text-transform: uppercase;
-margin: 0 0 24px 0;
+    color: #6E7E91;
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 1.3;
+    text-transform: uppercase;
+    margin: 0 0 24px 0;
 `;
 
 export const Mail = styled.a`
-font-weight: 900;
-font-size: 32px;
-line-height: 39px;
-color: #252525;
-text-decoration: none;
+    font-weight: 900;
+    font-size: 32px;
+    line-height: 39px;
+    color: #252525;
+    text-decoration: none;
 `;
 
 export const Text = styled.p`
-font-size: 18px;
-line-height: 1.4;
+    font-size: 18px;
+    line-height: 1.4;
 `;
 
 export const SocialLinks = styled.div`
-display:flex;
-gap: 24px;
-margin-top: 56px;
+    display:flex;
+    gap: 24px;
+    margin-top: 56px;
 `;
 
 export const Link = styled.a`
