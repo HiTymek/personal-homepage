c8896a387f13645965dcffbaf46a3f9b4fc3afd4
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-11-15 22:02:29 +0100

    added projectsSaga

---
+++ src/features/Projects/projectsSaga.js
@@ -0,0 +1,17 @@
+import { delay, put, takeEvery } from "redux-saga/effects";
+import { getProjects } from "./getProjects";
+import { fetchProjects, fetchProjectsError, fetchProjectsSuccess } from "./projectsSlice";
+
+export function* watchFetchProjectsHandler() {
+    try {
+        yield delay(2000);
+        const projects = yield call(getProjects);
+        yield put(fetchProjectsSuccess(projects));
+    } catch (error) {
+        yield put(fetchProjectsError);
+    }
+};
+
+export function* watchFetchProjects() {
+    yield takeEvery(fetchProjects.type, watchFetchProjectsHandler);
+};
