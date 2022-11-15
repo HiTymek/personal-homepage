2cf446a12ac982d026af75f14f7bf3a8d61c8f81
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-11-15 22:09:08 +0100

    added new reducer to store

--- src/store.js
+++ src/store.js
@@ -1,17 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "@redux-saga/core";
 import themeReducer from "./themeSlice";
-import { themeSaga } from "./themeSaga";
+import projectsReducer from "./features/Projects/projectsSlice";
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         theme: themeReducer,
+        projects: projectsReducer,
     },
     middleware: [sagaMiddleware],
 });
 
-sagaMiddleware.run(themeSaga);
+sagaMiddleware.run(rootSaga);
 
 export default store;
