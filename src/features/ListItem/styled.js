161315123858546c7eeb52342e62287990783a7e
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-11-17 18:05:30 +0100

    added media-queries to ListItem

--- src/features/ListItem/styled.js
+++ src/features/ListItem/styled.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
+import { device } from "../../theme";
 
 export const ListItem = styled.li`
     display: flex;
     color: ${({ theme }) => theme.color.text};
     font-size: 18px;
     line-height: 1.4;
+
+    @media ${device.tablet} {
+        font-size: 14px;
+    };
 `;
 
 export const Dot = styled.div`
@@ -14,4 +19,9 @@ export const Dot = styled.div`
     height: 9px;
     margin-right: 16px;
     background-color: ${({ theme }) => theme.color.primary};
+
+    @media ${device.tablet} {
+       width: 6px;
+       height: 6px;
+    };
 `;
