29bfeff028ef2d96c38e79169da4ea7d9f97c41b
HiTymek <wojcik.tymoteusz1999@gmail.com>
2022-11-17 19:24:41 +0100

    added media-queries to Container

--- src/features/Container/styled.js
+++ src/features/Container/styled.js
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import { device } from "../../theme";
 
 export const Container = styled.div`
     max-width: 1216px;
     margin: 115px auto auto auto;
+
+    @media ${device.tablet} {
+        margin-top: 33px;
+    };
 `;
